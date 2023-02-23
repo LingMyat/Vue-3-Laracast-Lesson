a216b3670f09075e2b9955e89ec628049cf305a7
LingMyat223 <lingmyataung@outlook.com>
2023-02-23 17:33:28 +0630

    Lesson 6

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,13 +1,26 @@
-export default {
+export default{
     template: `
-    <button class="bg-gray-200 hover:bg-gray-400 hover:text-white border rounded px-5 py-2 disabled:cursor-not-allowed" :disabled="processing">
+    <button 
+    :class="{
+        'border rounded px-5 py-2 disabled:cursor-not-allowed': true,
+        'bg-blue-600 hover:bg-blue-800 ': type === 'primary',
+        'bg-purple-600 hover:bg-purple-800 ': type === 'secondary',
+        'bg-gray-600 hover:bg-gray-800 ': type === 'muted',
+        'is-loading':processing,
+    }" 
+    :disabled="processing"
+    >
         <slot></slot>
     </button>
     `,
-
-    data() {
-        return {
-            processing: true,
-        };
-    }
+    props: {
+        type: {
+            type: String,
+            default: 'primary'
+        },
+        processing: {
+            type: Boolean,
+            default: false
+        }
+    },
 }
